d84634a8fa4e4b9d686e43e236b1eb377a89342e
Danail Boyukliev <danailboyukliev@gmail.com>
2026-06-15 22:00:26 +0300

    refactor: convert Repository from singleton object to Hilt-injectable class

--- app/src/main/java/com/sap/codelab/repository/App.kt
+++ app/src/main/java/com/sap/codelab/repository/App.kt
@@ -4,9 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-internal class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Repository.initialize(this)
-    }
-}
+internal class App : Application()
